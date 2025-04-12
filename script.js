7ce747e18eb3c33ce3473ecbbe2e10a905dd157f
Maria Govorukhina <m.govoru@yandex.ru>
2025-04-12 20:23:00 +0500

    faat: add initial project layout

---
+++ script.js
@@ -0,0 +1,17 @@
+const header = document.getElementById('header');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+});
+document.addEventListener('DOMContentLoaded', function() {
+  const burgerBtn = document.querySelector('.burger-menu-btn');
+  const menu = document.querySelector('.menu');
+
+  burgerBtn.addEventListener('click', function() {
+    menu.classList.toggle('open');
+  });
+});
+
